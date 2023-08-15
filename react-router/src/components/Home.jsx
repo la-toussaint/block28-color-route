abd08445bd514206e73bdd16c341463ea147e448
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-08-15 14:01:48 -0500

    updates with footer installed

---
+++ react-router/src/components/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+    <div className="home"><h1>Home</h1></div>
+    );
+    }
---
+++ react-router/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+
+
+// export default function NavBar() {
+//     return (
+
+// <div id="navbar">
+//     <Link to="/home">Home</Link>
+//     <Link to="/red">Red</Link>
+//     <Link to="/blue">Blue</Link>
+//       </div>
+//     )}
