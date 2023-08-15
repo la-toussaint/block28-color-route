58aecfc7b28110aa0387bee7f68969880f380625
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-08-15 14:28:22 -0500

    updated with footer

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -6,9 +6,10 @@ import Home from "./components/Home"
 
 export default function App() {
 
+
 return (
 <div id="container">
-      <h1>Hello React Router!</h1>
+      <h1>React Routing Colors!</h1>
       <div id="navbar">
     <Link to="/home">Home</Link>
     <Link to="/red">Red</Link>
@@ -20,6 +21,13 @@ return (
         <Route path="/red" element={<Red />} />
         <Route path="/blue" element={<Blue />} />
       </Routes>
+      </div>
+      <div>
+        <footer className="footer">
+        <Link to="/home">Home</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/blue">Blue</Link>
+        </footer>
       </div>
     </div>
 )}
